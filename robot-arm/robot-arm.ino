43192414931b82d1d68987e9381bdf06fc50c4b7
Simon Narang <simonarang202@gmail.com>
2017-01-26 19:25:45 -0500

    Connect to Serial Monitor
    This commit has this code receive commands in the form of characters
    such as ‘f’ to indicate forward. These commands go to the Arduino and
    cause
    the robot to do certain things via the serial monitor.

--- robot-arm/robot-arm.ino
+++ robot-arm/robot-arm.ino
@@ -3,22 +3,105 @@
 
 int message = 0;     //  This will hold one byte of the serial message
 
+bool forward;
+bool backward;
+bool up;
+bool down;
+bool left;
+bool right;
+bool clawClose;
+bool clawOpen;
+bool pennyFlipperAwake;
+bool pennyFlipperAsleep;
+
 vexMotor base;
+vexMotor hip;
+vexMotor shoulder;
+vexMotor claw;
+Servo pennyFlipper;
 
 void setup() {  
   Serial.begin(9600);  //set serial to 9600 baud rate
+  
   base.attach(3);
+  hip.attach(5);
+  shoulder.attach(6);
+  claw.attach(9);
+  pennyFlipper.attach(10);
+
+  forward = false;
+  backward = false;
+  up = false;
+  down = false;
+  left = false;
+  right = false;
+  clawClose = false;
+  clawOpen = false;
+  pennyFlipperAwake = false;
+  pennyFlipperAsleep = false;
+  
+  Serial.println("YAY Robot Up and Running");
 }
 
 void loop(){
     if (Serial.available() > 0) { //  Check if there is a new message
       message = Serial.read();    //  Put the serial input into the message
       if (message == 'l') {
-        base.write(-100);
+        if (left == false) {
+          left = true;
+          }
+        if (left == true) {
+          left = false;
+          }
         }
       if (message == 'r') {
-        base.write(100);
+        if (right == false) {
+          right = true;
+          }
+        if (right == true) {
+          right = false;
+          }
+        }
+      if (message == 'u') {
+        if (up == false) {
+          up = true;
+          }
+        if (up == true) {
+          up = false;
+          }
+        }
+      if (down == 'd') {
+        if (down == false) {
+          down = true;
+          }
+        if (down == true) {
+          down = false;
+          }
+        }
+      if (message == 'f') {
+        if (forward == false) {
+          forward = true;
+          }
+        if (forward == true) {
+          forward = false;
+          }
+        }
+      if (message == 'b') {
+        if (backward == false) {
+          backward = true;
+          }
+        if (backward == true) {
+          backward = false;
+          }
+        }
+      if (message == 'a') {
+        if (pennyFlipperAwake == false) {
+          pennyFlipperAwake = true;
+          }
+        if (pennyFlipperAwake == true) {
+          pennyFlipperAwake = false;
+          }
         }
       }
-}
+  }
 
